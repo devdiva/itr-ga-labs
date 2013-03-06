835e8340dcc06eec9b8e9b9735afb3e48383f6bf
Hari Khalsa <hari@devdiva.com>
2013-03-05 23:44:55 -0800

    Renamed homework files and added instructions for homework's 1-5.

---
+++ 01_ITR_HW_RUBY/HW_04.rb
@@ -0,0 +1,36 @@
+###############################################################################
+#
+# Introduction to Ruby on Rails
+#
+# HW 04
+#
+# Purpose:
+#
+# Read the links below and complete the exercises in this file. This Lab
+# will help you to review the basics of Object-Oriented Programming that
+# we learned in Lesson 04.
+#
+###############################################################################
+#
+# 1. Review your solution to Lab 03. Copy and Paste your solution to
+#    this file.
+#
+# 2. Create a new method called `increment_guess_count` that takes
+#    an Integer parameter and increments it by 1.
+#
+# 3. Create a new method called `guesses_left` that accepts an Integer
+#    parameter. Inside of it, write math so that you can tell how many
+#    guesses out of 3 that the Player has left. Use this new method
+#    in your code to tell the user how many guesses they have left.
+#
+# 4. Make sure to remove your local variable `guesses_left` and use the
+#    new method instead.
+#
+# 5. Make sure to comment your code so that you have appropriate
+#    documentation for you and for the TAs grading your homework. :-)
+#
+###############################################################################
+#
+# Student's Solution
+#
+###############################################################################
