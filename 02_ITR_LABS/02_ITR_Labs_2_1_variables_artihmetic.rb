b8bf2c5e92a46ede8baca5c1fe8b2c14ececa9aa
Hari Khalsa <hari@devdiva.com>
2013-03-05 23:46:07 -0800

    ITR Session 2 Labs.

---
+++ 02_ITR_LABS/02_ITR_Labs_2_1_variables_artihmetic.rb
@@ -0,0 +1,46 @@
+################################################################################
+#
+# Introduction to Ruby on Rails
+#
+# Labe 1: Variables and Arithmetic Exercise
+#
+################################################################################
+#
+# Below are 3 variables that we will use in this exercise: a, b, and c
+#
+################################################################################
+
+# 1. Create three variables, 'a', 'b', and 'c' and assign values to each 4, 2, and 5 respectively.
+
+a = 4
+b = 2
+c = 5
+
+# 2. Translate a^2 + c to Ruby code so that when printed, the result is 21
+
+
+# 3. Translate a x b + c into Ruby so that the result is 13
+ 
+ 
+# 4. Change the code in #3 to display the result 28 by adding parenthesies
+
+ 
+# 5. Display the result of 2 / 5 using the variables
+
+
+# 6. Display the result of 2.0 / 5 using vairables
+
+
+# 7. What are the differences between using an Integer versus a Float?
+
+
+# 8. Below are four expressions: two displaying even numbers modulus 2
+#    and two displaying odd numbers modules 2.
+puts 4 % 2
+puts 414 % 2
+puts 9 % 2
+puts 1311 % 2
+
+# 9. Do you notice a pattern with modulus 2 when dealing with even versus
+#    odd numbers?
+#
---
+++ 02_ITR_LABS/02_ITR_Labs_2_4_data_types_strings.rb
@@ -0,0 +1,53 @@
+################################################################################
+#
+# Introduction to Ruby on Rails
+#
+# Lab 2.4: Data Types and Strings
+#
+################################################################################
+
+# 1. Create 4 new variables, `month`, `day`, `day_of_week` and `year`.
+#    Assign each variable appropriately for today's date.
+#
+#    Example: Tuesday, September 19, 1985
+#
+#    `Month` should be a Symbol, `day` should be an Integer, `day_of_week`
+#    should be a String, and `year` an Integer.
+
+
+# 2. Construct a sentence of Today's date by concatenating the above
+#    variables like the example. Remember to cast appropriately.
+
+
+
+# 3. Now, construct the same sentence using String interpolation
+#    instead!
+#
+#    Hint: Do you still need to cast your variables to Strings?
+
+
+
+# 4. Let's try and mix things up a bit! Now we are going to take input
+#    from the command line and change it so that we can display whatever
+#    date the User would like to display.
+#
+#    prompt the user to enter the day of the week, and store the response in a variable day_of_week
+
+
+
+# 5. Now ask for the month and capture that data in the variable month.
+
+
+
+# 6. Ask for the day and capture that data in the variable day.
+
+
+
+# 7. And finally, do the same for the year!
+
+
+
+# 8. Use String interpolation to display the inputted full date to the user.
+
+
+
