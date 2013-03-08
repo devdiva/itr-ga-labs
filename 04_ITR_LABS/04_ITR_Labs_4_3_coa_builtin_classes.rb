a974ae780b9f0e976b5542595c6bffa1316dbc10
Hari Khalsa <hari@devdiva.com>
2013-03-07 16:52:19 -0800

    Lesson 4 Labs.

---
+++ 04_ITR_LABS/04_ITR_Labs_4_3_coa_builtin_classes.rb
@@ -0,0 +1,21 @@
+################################################################################
+#
+# Introduction to Ruby on Rails
+#
+# Code Along Built In Classes
+#
+################################################################################
+#We are going to use the built in class 'File' to create a .txt file and add some text to it. 
+
+# 1. Ask the user for a file name. Open or create a file by that name, 
+# by adding file extension".txt" to the users input.
+# Ask the user to write a sentence. Append that sentence to the file, preceded
+# by the current date and time. Use newline character ("\n") as needed. Be sure to close the file. 
+# Check your work on the file system.
+
+
+
+# 2. Use the File class to open the file created above (using the same input from above), to read and 
+# display its contents to the terminal window. Read documentation on the .gets method, and write the 
+# necessary loop code.
+
