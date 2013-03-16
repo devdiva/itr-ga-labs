420ac3a62c6fba8f3cbddd57afb4cbdacc426cb8
Hari Khalsa <hari@devdiva.com>
2013-03-16 11:45:22 -0700

    Lab 2.6, fixed number to match lab instructions.

--- 02_ITR_LABS/02_ITR_Labs_2_6_nested_conditionals.rb
+++ 02_ITR_LABS/02_ITR_Labs_2_6_nested_conditionals.rb
@@ -10,10 +10,14 @@
 #
 ################################################################################
 
+# 1. Create two variables and assign values as follows: 
+#   programs_day_of_week = "tuesday"
+#   programs_number = 3
+
 programs_day_of_week = "tuesday"
 programs_number = 3
 
-# 1. Let's get input from our User and see if they can guess which day
+# 2. Let's get input from our User and see if they can guess which day
 #    of the week we are thinking of.
 #
 #    Ask the User, "What day of the week am I thinking of?" and capture
@@ -21,13 +25,13 @@ programs_number = 3
 
 
 
-# 2. By default, `programs_day_of_week` is set to Tuesday. Let's compare
+# 3. By default, `programs_day_of_week` is set to Tuesday. Let's compare
 #    our variable to what the User inputted. If they got it right, tell
 #    them "Correct!" otherwise tell them "Wrong!"
 
 
 
-# 3. Now let's try seeing if two things are true at the same time.
+# 4. Now let's try seeing if two things are true at the same time.
 #
 #    Ask your User for a day of the week and then for a number. Remember
 #    to cast your User's input appropriately when comparing values!
@@ -43,7 +47,7 @@ programs_number = 3
 #    Otherwise, tell them "Wrong!"
 
 
-# 4. One last time, ask again for a day of the week and a number, except
+# 5. One last time, ask again for a day of the week and a number, except
 #    this time only one of them needs to be correct to "win"! But if
 #    they don't guess either correctly, they "lose"!
 
